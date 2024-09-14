b22f5f198cfcd3a0a9421bdb50ae7ea968e89bfe
Andrii Hulii <andrii.hulii@tietoevry.com>
2024-09-14 17:36:42 +0300

    Refactor push button code

--- nano_push_button_external_pull_up/nano_push_button_external_pull_up.ino
+++ nano_push_button_external_pull_up/nano_push_button_external_pull_up.ino
@@ -1,11 +1,10 @@
-// Turns on LED connected to digital pin 2,
-// when pressing the push button attached to diginal pin 7.
-// - when the button is not pressed button pin is pulled HIGH
-// - when the button is pressed the input pin is pulled LOW
-int ledPin = 2;
-int ledOnTime = 2000;
-int ledOffTime = 1000;
+/* Turns on LED connected to digital pin 2,
+when pressing the push button attached to digital input pin 7.
 
+- when the button is not pressed input pin is pulled HIGH
+- when the button is pressed the input pin is pulled LOW
+*/
+int ledPin = 2;
 int buttonPin = 7;
 int buttonState = 0;
 
@@ -21,5 +20,4 @@ void loop() {
   } else {
       digitalWrite(ledPin, LOW); 
   }
-
 }
